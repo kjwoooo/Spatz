8deee7edcec4501fbaf4267073c6300e8d0f4bce
chohyeyeon <heyyeon77@naver.com>
2024-08-04 16:28:07 +0900

    feat: 관리자 신고 ru & 정지 c (예외처리적용X)

--- src/main/java/com/elice/spatz/domain/userfeature/controller/AdminFeatureController.java
+++ src/main/java/com/elice/spatz/domain/userfeature/controller/AdminFeatureController.java
@@ -1,4 +1,39 @@
 package com.elice.spatz.domain.userfeature.controller;
 
+import com.elice.spatz.domain.userfeature.model.dto.response.ReportDto;
+import com.elice.spatz.domain.userfeature.model.entity.Report;
+import com.elice.spatz.domain.userfeature.model.entity.ReportStatus;
+import com.elice.spatz.domain.userfeature.service.AdminFeatureService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+@RestController
+@RequiredArgsConstructor
 public class AdminFeatureController {
+    private final AdminFeatureService adminFeatureService;
+
+    // 1. 신고 목록 조회
+    @GetMapping("admin/reports")
+    public ResponseEntity<Page<ReportDto>> getWaitingReports(@RequestParam ReportStatus reportStatus, @PageableDefault(page=0, size=10, sort="createdAt", direction = Sort.Direction.DESC) Pageable pageable) {
+        Page<ReportDto>  waitingReportDtos = adminFeatureService.getWaitingReports(reportStatus, pageable);
+        return ResponseEntity.ok(waitingReportDtos);
+    }
+    // 2. 신고 상세 조회
+    @GetMapping("admin/reports/{reportId}")
+    public ResponseEntity<ReportDto> getReport(@PathVariable Long reportId) {
+        ReportDto reportDto = adminFeatureService.getWaitingReport(reportId);
+        return ResponseEntity.ok(reportDto);
+    }
+    // 3. 신고 응답
+    @PatchMapping("admin/reports/{reportId}")
+    public ResponseEntity<String> responseReport(@PathVariable Long reportId, @RequestParam ReportStatus reportStatus) {
+        adminFeatureService.responseReport(reportId, reportStatus);
+        return ResponseEntity.ok("신고 수정이 완료되었습니다");
+    }
 }
--- src/main/java/com/elice/spatz/domain/userfeature/service/AdminFeatureService.java
+++ src/main/java/com/elice/spatz/domain/userfeature/service/AdminFeatureService.java
@@ -1,4 +1,78 @@
 package com.elice.spatz.domain.userfeature.service;
 
+import com.elice.spatz.domain.user.entity.Users;
+import com.elice.spatz.domain.userfeature.model.dto.response.ReportDto;
+import com.elice.spatz.domain.userfeature.model.dto.response.ResponseMapper;
+import com.elice.spatz.domain.userfeature.model.entity.BannedUser;
+import com.elice.spatz.domain.userfeature.model.entity.Report;
+import com.elice.spatz.domain.userfeature.model.entity.ReportCount;
+import com.elice.spatz.domain.userfeature.model.entity.ReportStatus;
+import com.elice.spatz.domain.userfeature.repository.BannedUserRepository;
+import com.elice.spatz.domain.userfeature.repository.ReportCountRepository;
+import com.elice.spatz.domain.userfeature.repository.ReportRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
 public class AdminFeatureService {
+    private final ReportRepository reportRepository;
+    private final ReportCountRepository reportCountRepository;
+    private final ResponseMapper responseMapper;
+    private final BannedUserRepository bannedUserRepository;
+
+    // 1. 신고 목록 조회
+    @Transactional
+    public Page<ReportDto> getWaitingReports(ReportStatus reportStatus, Pageable pageable){
+        Page<Report> waitingReports = reportRepository.findAllByReportStatus(reportStatus, pageable);
+        List<ReportDto> waitingReportDtoList = waitingReports.getContent().stream()
+                .map(responseMapper::reportToReportDto)
+                .collect(Collectors.toList());
+        return new PageImpl<>(waitingReportDtoList, pageable, waitingReports.getTotalElements());
+    }
+    // 2. 신고 상세 조회
+    @Transactional
+    public  ReportDto getWaitingReport(long reportId){
+        Report report = reportRepository.findById(reportId).orElse(null);
+        return responseMapper.reportToReportDto(report);
+    }
+    // 3. 신고 응답
+    @Transactional
+    public void responseReport(long reportId, ReportStatus reportStatus){
+        Report report = reportRepository.findById(reportId).orElseThrow();
+
+        report.setReportStatus(reportStatus);
+
+        // 신고 수락시 누적 신고 횟수 추가
+        if (reportStatus == ReportStatus.ACCEPTED){
+            ReportCount reportCount = reportCountRepository.findByUserId(report.getReported().getId());
+            if (reportCount == null) {
+                ReportCount newReportCount = new ReportCount(report.getReported(), 1);
+                reportCountRepository.save(newReportCount);
+            }
+            else {
+                reportCount.setReportCount(reportCount.getReportCount()+1);
+                // 누적 신고 횟수 3회이면 정지
+                int reportedReportCount =  reportCount.getReportCount();
+                if (reportedReportCount == 3){
+                    LocalDateTime now = LocalDateTime.now();
+                    LocalDateTime oneMonthLater = now.plusMonths(1);
+
+                    BannedUser newBannedUser = new BannedUser(report.getReported(), now, oneMonthLater);
+                    bannedUserRepository.save(newBannedUser);
+                }
+            }
+        }
+    }
+
+    // 사용자 정지 해제 스케줄러
+
 }
