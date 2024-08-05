a0b0fec201cc1d6d4b159883f73a31a4f471a520
chohyeyeon <heyyeon77@naver.com>
2024-08-05 11:39:32 +0900

    feat: 정지 해제 스케줄러 (예외처리적용X)

--- src/main/java/com/elice/spatz/domain/userfeature/repository/BannedUserRepository.java
+++ src/main/java/com/elice/spatz/domain/userfeature/repository/BannedUserRepository.java
@@ -4,6 +4,10 @@ import com.elice.spatz.domain.userfeature.model.entity.BannedUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 @Repository
 public interface BannedUserRepository extends JpaRepository<BannedUser, Long> {
+    List<BannedUser> findAllByBannedEnd(LocalDateTime bannedEnd);
 }
---
+++ src/main/java/com/elice/spatz/domain/userfeature/service/ReactiveBannedUserScheduler.java
@@ -0,0 +1,44 @@
+package com.elice.spatz.domain.userfeature.service;
+
+import com.elice.spatz.domain.user.entity.Users;
+import com.elice.spatz.domain.user.repository.UserRepository;
+import com.elice.spatz.domain.userfeature.model.entity.BannedUser;
+import com.elice.spatz.domain.userfeature.repository.BannedUserRepository;
+import com.elice.spatz.domain.userfeature.repository.ReportCountRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Component
+@Transactional
+@RequiredArgsConstructor
+public class ReactiveBannedUserScheduler {
+    private final BannedUserRepository bannedUserRepository;
+    private final ReportCountRepository reportCountRepository;
+    private final UserRepository userRepository;
+
+    @Scheduled(cron = "0 0 0 * * ?")
+    public void reactiveBannedUsers() {
+        LocalDateTime now = LocalDateTime.now().minusSeconds(1);
+
+        List<BannedUser> bannedUserList = bannedUserRepository.findAllByBannedEnd(now);
+
+        bannedUserList.stream()
+                .forEach(bannedUser -> {
+                    Users user = bannedUser.getUser();
+
+                    bannedUserRepository.delete(bannedUser);
+                    if(user.getReportCount() != null){
+                        reportCountRepository.delete(user.getReportCount());
+                    }
+                    user.setBannedUser(null);
+                    user.setReportCount(null);
+                    userRepository.save(user);
+        });
+    }
+}
+
