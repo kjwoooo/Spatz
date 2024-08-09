20e84a3230fead7a87b80d8ff1eafded607bb9ac
chohyeyeon <heyyeon77@naver.com>
2024-08-09 14:41:20 +0900

    refactor: 관리자 api 앤드포인트 변경

--- src/main/java/com/elice/spatz/domain/userfeature/controller/AdminFeatureController.java
+++ src/main/java/com/elice/spatz/domain/userfeature/controller/AdminFeatureController.java
@@ -17,19 +17,19 @@ public class AdminFeatureController {
     private final AdminFeatureService adminFeatureService;
 
     // 1. 신고 목록 조회
-    @GetMapping("admin/reports")
+    @GetMapping("/admin/reports")
     public ResponseEntity<Page<ReportDto>> getWaitingReports(@RequestParam ReportStatus reportStatus, @PageableDefault(page=0, size=10, sort="createdAt", direction = Sort.Direction.DESC) Pageable pageable) {
         Page<ReportDto>  waitingReportDtos = adminFeatureService.getWaitingReports(reportStatus, pageable);
         return ResponseEntity.ok(waitingReportDtos);
     }
     // 2. 신고 상세 조회
-    @GetMapping("admin/reports/{reportId}")
+    @GetMapping("/admin/reports/{reportId}")
     public ResponseEntity<ReportDto> getReport(@PathVariable Long reportId) {
         ReportDto reportDto = adminFeatureService.getWaitingReport(reportId);
         return ResponseEntity.ok(reportDto);
     }
     // 3. 신고 응답
-    @PatchMapping("admin/reports/{reportId}")
+    @PatchMapping("/admin/reports/{reportId}")
     public ResponseEntity<String> responseReport(@PathVariable Long reportId, @RequestParam ReportStatus reportStatus) {
         adminFeatureService.responseReport(reportId, reportStatus);
         return ResponseEntity.ok("신고 수정이 완료되었습니다");
