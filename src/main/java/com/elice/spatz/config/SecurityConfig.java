470194540a3865a82269fa894e3c9b0872a9889e
donghee <donghee@donghees-MacBook-Air.local>
2024-08-12 11:41:04 +0900

    Fix : CORS 설정 변경

--- src/main/java/com/elice/spatz/config/SecurityConfig.java
+++ src/main/java/com/elice/spatz/config/SecurityConfig.java
@@ -73,7 +73,6 @@ public class SecurityConfig {
                     public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
                         CorsConfiguration config = new CorsConfiguration();
                         config.setAllowedOrigins(Collections.singletonList("http://localhost:5173"));
-                        config.setAllowedOrigins(Collections.singletonList("http://localhost:3000"));
                         config.setAllowedMethods(Collections.singletonList("*"));
                         config.setAllowCredentials(true);
                         config.setAllowedHeaders(Collections.singletonList("*"));
