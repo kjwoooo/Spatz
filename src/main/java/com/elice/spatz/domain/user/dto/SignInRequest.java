12ab058e77e2449f8daf3589c5e4c4f2fba657ca
donghee <donghee@donghees-MacBook-Air.local>
2024-08-14 09:43:45 +0900

    Feat : 로그인 시 입력값 검증 추가

--- src/main/java/com/elice/spatz/domain/user/dto/SignInRequest.java
+++ src/main/java/com/elice/spatz/domain/user/dto/SignInRequest.java
@@ -1,11 +1,19 @@
 package com.elice.spatz.domain.user.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
 @AllArgsConstructor
 public class SignInRequest {
+
+    @NotBlank(message = "이메일은 필수 입력사항입니다")
+    @Email(message = "올바른 이메일 형식이 아닙니다")
     private String username;
+
+    @NotBlank(message = "비밀번호는 필수 입력사항입니다")
     private String password;
 }
