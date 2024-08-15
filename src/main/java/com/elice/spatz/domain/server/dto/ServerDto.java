88728b5b6c73727875c02842f49bb214b5197262
daehee719 <daehee719@gmail.com>
2024-08-16 02:58:54 +0900

    수정

--- src/main/java/com/elice/spatz/domain/server/dto/ServerDto.java
+++ src/main/java/com/elice/spatz/domain/server/dto/ServerDto.java
@@ -1,7 +1,10 @@
 package com.elice.spatz.domain.server.dto;
 
 import com.elice.spatz.domain.server.entity.Servers;
+import com.elice.spatz.domain.serverUser.entity.ServerUser;
 import lombok.*;
+
+import java.util.List;
 
 @Data
 @AllArgsConstructor
@@ -10,11 +13,15 @@ import lombok.*;
 public class ServerDto {
     private Long id;
     private String name;
+    private String inviteCode;
+    private List<ServerUser> serverUsers;
 
     public Servers toEntity(){
             return Servers.builder()
                     .id(this.id)
                     .name(this.name)
+                    .inviteCode(this.inviteCode)
+                    .serverUsers(this.serverUsers)
                     .build();
     }
 }
--- src/main/java/com/elice/spatz/domain/server/repository/ServerRepository.java
+++ src/main/java/com/elice/spatz/domain/server/repository/ServerRepository.java
@@ -3,7 +3,8 @@ package com.elice.spatz.domain.server.repository;
 import com.elice.spatz.domain.server.entity.Servers;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
+import java.util.Optional;
 @Repository
 public interface ServerRepository extends JpaRepository<Servers, Long>  {
+    Optional<Servers> findByInviteCode(String inviteCode);
 }
